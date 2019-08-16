2de9e0215d1460f178c4c555978561333d4bbd33
Django Koenig <django.koenig@gmail.com>
2019-08-16 09:52:11 -0400

    clickable squares

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var origBoard;
+let origBoard;
 const human = 'O';
 const robot = 'X';
 const boardWins = [
@@ -11,70 +11,25 @@ const boardWins = [
   [0, 4, 8],
   [6, 4, 2]
 ] 
-const cells = document.querySelector('.cells');
+ 
+const cells = document.querySelectorAll('.cell');
 startGame();
 
 function startGame() {
   document.querySelector(".endgame").style.display = "none"; 
   origBoard = Array.from(Array(9).keys());
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+    cells[i].addEventListener('click', turnClick, false);
+  }
 }
 
-// -------------------OLD CODE------------------ //
+function turnClick(square) {
+  turn(square.target.id, human)
+}
 
-// let button = document.querySelector("#start");
-// button.addEventListener("click", (startTheGame) => {
-// alert('Let the games begin! Click on any square to start, player X.')
-// })
-// start button & inital "start game" alert
-
-// startButton.addEventListener("click", evt => {
-//   evt.target.disabled = true;
-//   const gameStatus = document.createElement("p");
-//   gameStatus.appendChild(gameStatus);
-//   setUpBoard();
-//   console.log("board is setup");
-// });
-// function setUpBoard() {
-//   const allCells = document.querySelectorAll('div, div div');
-//   allCells.forEach(evt =>
-//     evt.target.addEventListener('click', handeClick))
-// }
-// function handeClick(e) {
-//   e.target.textContent = 'x';
-//   console.log('clicked cell', e .target);
-// }
-
-// class Board {
-//   constructor(cells) {
-//     let rowOne = cells.slice(0, 3);
-//     let rowTwo = cells.slice(3, 6);
-//     let rowThree = cells.slice(6, 9);
-//     this.cells = [rowOne, rowTwo, rowThree];
-//   }
-// }
-
-// let letterX = '<img src=x-pic.jpg>'
-// let letterO = '<img src=o-pic.jpg>'
-// // add images for x and 0
-// const playerX = 'x';
-// const playerY= 'y';   
-
-// // while winConditions = false
-
-// //function for winConditions
-// const winConditions = [
-//   [0, 1, 2, "row-1"],
-//   [3, 4, 5, "row-2"],
-//   [6, 7, 8, "row-3"],
-//   [0, 3, 6, "col-1"],
-//   [1, 4, 7, "col-2"],
-//   [2, 5, 8, "col-3"],
-//   [0, 4, 8, "diag-1"],
-//   [2, 4, 6, "diag-2"]  
-// ];
-// const rows = document.querySelectorAll('.row')
-// //winConditions -> 8 ways to win
-// startGame();
-// function startGame() {
-//   document.querySelector(".endgame").getElementsByClassName.display = "none"
-// }
+function turn(squareId, player) {
+  origBoard[squareId] = player;
+  document.getElementById(squareId) =
+}
--- win-check2.js
+++ win-check2.js
@@ -1,63 +1,63 @@
-var gridOne = {
-  cells: [
-    ["x", "o", "x"],
-    ["o", "x", "o"],
-    ["x", "o", "o"]
-  ],
-  column: function(number) {
-    number = number - 1;
-    return [
-      this.cells[0][number],
-      this.cells[1][number],
-      this.cells[2][number]
-    ];
-  },
-  row: function(number) {
-    number = number - 1;
-    return this.cells[number];
-  },
-  diagonal: function(leftOrRight) {
-    if (leftOrRight === "left") {
-      return [
-        this.cells[0][0],
-        this.cells[1][1],
-        this.cells[2][2]
-      ];
-    } else {
-      return [
-        this.cells[0][2],
-        this.cells[1][1],
-        this.cells[2][0]
-      ];
+// var gridOne = {
+//   cells: [
+//     ["x", "o", "x"],
+//     ["o", "x", "o"],
+//     ["x", "o", "o"]
+//   ],
+//   column: function(number) {
+//     number = number - 1;
+//     return [
+//       this.cells[0][number],
+//       this.cells[1][number],
+//       this.cells[2][number]
+//     ];
+//   },
+//   row: function(number) {
+//     number = number - 1;
+//     return this.cells[number];
+//   },
+//   diagonal: function(leftOrRight) {
+//     if (leftOrRight === "left") {
+//       return [
+//         this.cells[0][0],
+//         this.cells[1][1],
+//         this.cells[2][2]
+//       ];
+//     } else {
+//       return [
+//         this.cells[0][2],
+//         this.cells[1][1],
+//         this.cells[2][0]
+//       ];
 
-    }
-  }
-};
+//     }
+//   }
+// };
 
-assertTrue("Column Zero is 'x', 'o', 'x'",
-  arrayCompare(gridOne.column(1), ['x', 'o', 'x']));
+// assertTrue("Column Zero is 'x', 'o', 'x'",
+//   arrayCompare(gridOne.column(1), ['x', 'o', 'x']));
 
-assertTrue("Should break",
-  arrayCompare(['o', 'o', 'o'], ['x', 'o', 'x']));
+// assertTrue("Should break",
+//   arrayCompare(['o', 'o', 'o'], ['x', 'o', 'x']));
 
-assertTrue("String one is String one", '1' === '1')
+// assertTrue("String one is String one", '1' === '1')
 
-function arrayCompare(first, second) {
-  if (first.length !== second.length) { return false; }
-  for (let index = 0; index < first.length; index++) {
-    const firstValue = first[index];
-    const secondValue = second[index];
-    if (firstValue !== secondValue) { return false };
-  }
-  return true;
-}
+// function arrayCompare(first, second) {
+//   if (first.length !== second.length) { return false; }
+//   for (let index = 0; index < first.length; index++) {
+//     const firstValue = first[index];
+//     const secondValue = second[index];
+//     if (firstValue !== secondValue) { return false };
+//   }
+//   return true;
+// }
 
-function assertTrue(message, value) {
-  if (value) {
-    console.log(`Success: ${message}`);
-    return true;
-  } else {
-    console.log(`Failure: ${message}`);
-    return false;
-  }
-}s
+// function assertTrue(message, value) {
+//   if (value) {
+//     console.log(`Success: ${message}`);
+//     return true;
+//   } else {
+//     console.log(`Failure: ${message}`);
+//     return false;
+//   }
+// }s
