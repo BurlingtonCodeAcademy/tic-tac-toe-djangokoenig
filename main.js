e5720e462f8e013338475dfbf76bb501da1462a5
Django Koenig <django.koenig@gmail.com>
2019-08-17 00:54:04 -0400

    basic ai + winner + tie messages

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let origBoard;
 const human = 'O';
 const robot = 'X';
-const boardWins = [
+const winCombos = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -26,10 +26,64 @@ function startGame() {
 }
 
 function turnClick(square) {
-  turn(square.target.id, human)
+  if (typeof origBoard[square.target.id] == 'number') {
+    turn(square.target.id, human)
+    if (!checkTie()) turn(bestSpot(), robot);
+  }
 }
 
 function turn(squareId, player) {
   origBoard[squareId] = player;
-  document.getElementById(squareId) =
+  document.getElementById(squareId).innerText = player;
+  let gameWon = checkWin(origBoard, player)
+  if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) =>
+    (e === player) ? a.concat(i) : a, []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = {index: index, player: player};
+      break;
+    }
+  }
+  return gameWon;
+}
+//check which player has won. human=blue. robot=tomato. Remove event listeners.
+function gameOver(gameWon) {
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor = 
+      gameWon.player == human ? "slateblue" : "tomato";
+  }
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener('click', turnClick, false);
+  }
+  declareWinner(gameWon.player == human ? "You win!" : "Robots win!")
+}
+
+function declareWinner(who) {
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+  return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+  return emptySquares()[0];
+}
+
+function checkTie() {
+  if (emptySquares().length == 0) {
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = "darkorange";
+      cells[i].removeEventListener('click', turnClick, false);
+    }
+    declareWinner("Tie Game!")
+    return true;
+  }
+  return false;
 }
